536c15ab7dcb12449fdb395d2b972928a0ed1335
Mohamad Hossein <mhyamininia@gmail.com>
2019-01-23 18:20:39 +0330

    add Const var to Const Class

---
+++ app/src/main/java/com/example/mohammad/samplemyapp/network/Const.java
@@ -0,0 +1,8 @@
+package com.example.mohammad.samplemyapp.network;
+
+public class Const {
+    static final String BASE_URL = "https://api.foursquare.com/v2/";
+    public static final String CLIENT_ID = "KAURLHSXIUVHAL5UWUIHSNH14PAETE0MGOEVVIMWA4VAATUQ";
+    public static final String CLIENT_SECRET = "QIFPEJBCUPCS0J1JNEGUQPLRRRF4I3UNV4GWO5UXOOB23XOY";
+    public static final String VERSION = "20180323";
+}
