0d3f0aadad40e551cb8a9d8a7c1ad48fd52a59b9
Mohamad Hossein <mhyamininia@gmail.com>
2019-01-23 18:19:06 +0330

    add getPlaceList func

--- app/src/main/java/com/example/mohammad/samplemyapp/network/ApiInterface.java
+++ app/src/main/java/com/example/mohammad/samplemyapp/network/ApiInterface.java
@@ -1,4 +1,17 @@
 package com.example.mohammad.samplemyapp.network;
 
+import com.example.mohammad.samplemyapp.object.BaseResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface ApiInterface {
+
+    @GET("venues/explore")
+    Call<BaseResponse> getPlaceList(@Query("client_id") String client_id,
+                                    @Query("secret_id") String secret_id,
+                                    @Query("ll") String ll,
+                                    @Query("v") String v);
+
 }
