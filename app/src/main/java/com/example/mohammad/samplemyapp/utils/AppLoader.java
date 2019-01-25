fc10641cfedc1ea82e94215d61a98660b27ef656
Mohamad <mhyamininia@gmail.com>
2019-01-25 16:26:50 +0330

    create Application Class.Write GetContext func

--- app/src/main/java/com/example/mohammad/samplemyapp/utils/AppLoader.java
+++ app/src/main/java/com/example/mohammad/samplemyapp/utils/AppLoader.java
@@ -1,12 +1,20 @@
 package com.example.mohammad.samplemyapp.utils;
 
 import android.app.Application;
+import android.content.Context;
 
 public class AppLoader extends Application {
+
+    private static Context mContext;
+
+    public static Context getAppContext() {
+        return mContext;
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        mContext=this;
 
     }
 }
