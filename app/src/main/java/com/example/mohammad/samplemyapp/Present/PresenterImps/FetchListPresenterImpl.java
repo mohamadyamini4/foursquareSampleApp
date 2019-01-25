c119dfcb9a838e21f311921fb5f6c0d0c4799552
Mohamad <mhyamininia@gmail.com>
2019-01-25 16:28:51 +0330

    check location more than 100 metr and save last location

--- app/src/main/java/com/example/mohammad/samplemyapp/Present/PresenterImps/FetchListPresenterImpl.java
+++ app/src/main/java/com/example/mohammad/samplemyapp/Present/PresenterImps/FetchListPresenterImpl.java
@@ -7,39 +7,67 @@ import com.example.mohammad.samplemyapp.Model.ModelImpls.getListModelImpl;
 import com.example.mohammad.samplemyapp.Present.Presenter;
 import com.example.mohammad.samplemyapp.View.View;
 import com.example.mohammad.samplemyapp.object.BaseResponse;
+import com.example.mohammad.samplemyapp.utils.AppLoader;
+import com.example.mohammad.samplemyapp.utils.PrefManager;
 
-public class FetchListPresenterImpl implements Presenter.fetchList ,Presenter.ReceivedPlaceList {
+import static com.example.mohammad.samplemyapp.network.Const.PREF_LAST_LOCATION;
+
+public class FetchListPresenterImpl implements Presenter.fetchList, Presenter.ReceivedPlaceList {
 
     View.fetchListManager viewFetchListManager;
     Model.modelFetchList modelFetchList;
 
     public FetchListPresenterImpl(View.fetchListManager fetchListManager) {
         this.viewFetchListManager = fetchListManager;
-        modelFetchList=new getListModelImpl(this);
+        modelFetchList = new getListModelImpl(this);
 
     }
 
 
     @Override
     public void getList(Location location) {
+        if (getLastLocation().equals("*") || moreThan100Meters(location)){
+            saveLocationtoPref(location);
+            modelFetchList.getList(convertLocationToString(location));
+        }
+    }
+    private String getLastLocation(){
+        return PrefManager.getInstance(AppLoader.getAppContext()).getSaveString(PREF_LAST_LOCATION);
+    }
 
-        modelFetchList.getList(convertLocationToString(location));
+    private void saveLocationtoPref(Location location){
+        PrefManager.getInstance(AppLoader.getAppContext()).saveString(PREF_LAST_LOCATION,convertLocationToString(location));
+    }
 
+    private boolean moreThan100Meters(Location location) {
+
+        if (location != null) {
+            String[] latlong = getLastLocation().split(",");
+            double latp1 = Double.parseDouble(latlong[0]);
+            double lngp1 = Double.parseDouble(latlong[1]);
+            Location lastLocation = new Location("p1");
+            lastLocation.setLatitude(latp1);
+            lastLocation.setLongitude(lngp1);
+            float distanceInMeters = location.distanceTo(lastLocation);
+            if (distanceInMeters > 100)
+                return true;
+        }
+
+        return false;
     }
 
     private String convertLocationToString(Location location) {
 
-        return String.format("%s,%s",location.getLatitude(),location.getLongitude());
+        return String.format("%s,%s", location.getLatitude(), location.getLongitude());
     }
 
     @Override
     public void receivedList(Object baseResponse) {
 
         if (baseResponse instanceof BaseResponse) {
-            if (((BaseResponse) baseResponse).getMeta().getCode()==200) {
+            if (((BaseResponse) baseResponse).getMeta().getCode() == 200) {
                 viewFetchListManager.receiveList(((BaseResponse) baseResponse).getResponse().getVenues());
-            }
-            else
+            } else
                 viewFetchListManager.onreceiveListErr();
         }
     }
